bade967b0b974daabc2ca75083be0a7938f997eb
NoamR <noam.raby@gmail.com>
2024-05-16 14:45:35 +0200

    serialize Response objects

--- src/logger.js
+++ src/logger.js
@@ -56,6 +56,8 @@ export function serialize(something) {
     return something.toString();
   } else if (something instanceof Promise) {
     return "Can't serialize a promise";
+  } else if (something instanceof Response) {
+    return `Request failed with ${something.status} (${something.statusText}). URL: ${something.url}`;
   }
   return JSON.stringify(something);
 }
